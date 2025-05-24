bc1f756ac9d70259c4de361217cf139f1af23d97
HourArrival <broadgifts@gmail.com>
2025-05-23 23:32:38 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div>
         {/* <div style={{textAlign: 'center',}}><img src={Mentor} alt="Mentor helping Mentee(s)" style={{width: '300px'}}></img></div> */}
-        <div className='App'><h1>Learn a skill, publish a new project, accomplish your favorite career.</h1></div>
+        <div className='App'><h1>Reach your goals with experienced mentors.</h1></div>
     </div>
   );
 }
